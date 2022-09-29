b6a19802d1606a9ee7c38102dbedef28871cc1d1
“izawasoma” <izawasoma@gmail.com>
2022-09-29 15:51:57 +0900

    useStateの処理を追加しました。

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import React ,{ useState } from "react";
+import "./styles.css";
 
-function App() {
+//状態変数countの初期状態
+const INITIAL_COUNT = 0;
+
+//状態変数nameの初期状態
+const INITIAL_NAME = "JavaScript";
+
+const SampleComponent = () => {
+  //useState: 状態を保持するcountと更新を行なう関数setCount
+  const [count, setCount] = useState(INITIAL_COUNT);
+
+  //useState: 状態を保持するnameと更新を行なう関数setName
+  const [name, setName] = useState(INITIAL_NAME);
+
+  //countを1ずつカウントアップを行なう
+  const countIncrement = () => {setCount((prevCount) => prevCount + 1)};
+
+  //countを1ずつカウントダウンする
+  const countDecrement = () => {setCount((prevCount) => prevCount - 1)};
+
+  //countの値を初期化する
+  const countReset = () => {setCount(INITIAL_COUNT)}
+
+  const handleChangeName = (e) => {
+    setName(e.target.value);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>
+        現在のカウント数：<b>{count}</b><br />
+        countの初期値：<b>{INITIAL_COUNT}</b>
+      </p>
+
+      <button onClick={countIncrement}>+1</button>
+      <button onClick={countDecrement}>-1</button>
+      <button onClick={countReset}>Reset</button>
+
+      <p>
+        Hello,<b>{name}!!</b><br />
+        nameの初期値：<b>{INITIAL_NAME}</b>
+      </p>
+
+      <input type="text" onChange={handleChangeName} />
     </div>
   );
 }
 
-export default App;
+export default function App() {
+  return (
+    <>
+      <SampleComponent></SampleComponent>
+    </>
+  );
+}
